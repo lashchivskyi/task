91a50952db1175e154194e0192e0dc271195d147
serhii <lashchivskyi@gmail.com>
2020-08-10 19:10:23 +0300

    Updates

---
+++ precache-manifest.e56f57a036138764414a918fea032ca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3afd4958c8521aa17126bcbf43dd7f",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "3455fa874c175bea16b7",
+    "url": "/task/static/css/main.28f25f50.chunk.css"
+  },
+  {
+    "revision": "14c3c0fb754fa7d19926",
+    "url": "/task/static/js/2.091134ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task/static/js/2.091134ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3455fa874c175bea16b7",
+    "url": "/task/static/js/main.4fa98fe0.chunk.js"
+  },
+  {
+    "revision": "6d1324646adf9796c2cd",
+    "url": "/task/static/js/runtime-main.06d77a8d.js"
+  }
+]);
